850c19c074263eaf59a0ea380fe195898d8b39e4
xiao ping <364878853@qq.com>
2018-01-07 14:19:14 +0800

    finish

---
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.itmuch.cloud.controller;
+
+import com.itmuch.cloud.entity.User;
+import com.itmuch.cloud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        User findOne = userRepository.findOne(id);
+        return findOne;
+    }
+}
