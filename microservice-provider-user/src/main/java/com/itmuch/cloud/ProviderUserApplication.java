54a7825c6351cc769971e447dba072aa1c82855e
xiao ping <364878853@qq.com>
2018-01-07 16:54:13 +0800

    finish1

---
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/ProviderUserApplication.java
@@ -0,0 +1,28 @@
+package com.itmuch.cloud;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProviderUserApplication {
+    @Bean(name = "dataSource")
+    public DataSource dataSource(Environment env) {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(env.getProperty("spring.datasource.url"));
+        ds.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        ds.setUsername(env.getProperty("spring.datasource.username"));
+        ds.setPassword(env.getProperty("spring.datasource.password"));
+        return ds;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderUserApplication.class, args);
+    }
+}
